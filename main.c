9d6199a4bc01a3db955a4a723aa85824358a9749
Mathias Schmitt <mathiaspeterhorst@gmail.com>
2020-08-13 19:40:02 +0200

    [DEV] chktropy: add round option.
    Display the number of possible passwords in -a option.

--- main.c
+++ main.c
@@ -3,53 +3,60 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <errno.h>
 
-#define BUF_SIZE 10
+#define BUF_SIZE 128
 #define ASCII_SIZE 128
 
 enum arguments {
-	ARGS_A = 0X01,
-	ARGS_I = 0x02,
-	ARGS_E = 0x04
+	ARGS_A = 0X01, /* Display all informations */
+	ARGS_I = 0x02, /* Do not print input */
+	ARGS_E = 0x04, /* Only print the entropy */
+	ARGS_R = 0x08  /* Round entropy */
 };
 
 static void usage(void)
 {
 	printf("Usage: chktropy [OPTIONS]\n");
 	printf("Calculate the entropy of the password given on the standard input\n");
+	printf("  -h  --help       display this help message\n");
 	printf("  -i  --stdin      do not print stdin while reading it\n");
-	printf("  -a  --all        display all informations\n");
-	printf("  -e  --entropy    display the entropy only\n");
+	printf("  -a  --all        display number of chars, unique chars,\n");
+	printf("                     and the number possible passwords\n");
+	printf("  -e  --entropy    display the entropy value only\n");
+	printf("  -r  --round      round the entropy to the closest integer\n");
 }
 
 static double calculate_entropy(
 	unsigned long int nb_unique_chars,
-	unsigned long int nb_chars)
+	unsigned long int nb_chars,
+	double *nb_passwords)
 {
-	double nb_possible_passwd = 0.0;
 	double entropy = 0.0;
 
-	// Gérer les cas de retours (overflow nombre, NaN)
-	nb_possible_passwd = pow((double)nb_unique_chars, (double)nb_chars);
-	// Gérer les cas de retours (overflow nombre, NaN)
-	entropy = log(nb_possible_passwd);
+	*nb_passwords = pow((double)nb_unique_chars, (double)nb_chars);
+
+	/* 0^0 = 1, so nb_passwords will never be 0 */
+	entropy = log(*nb_passwords);
+
+	*nb_passwords = round(*nb_passwords);
 
 	return entropy;
 }
 
 int main(int argc, char *argv[])
 {
-	char buffer[BUF_SIZE] = {0};
 	char ascii[ASCII_SIZE] = {0};
+	char buffer[BUF_SIZE] = {0};
+	char args = 0;
 	int opt;
 	unsigned long int nb_unique_chars = 0;
 	unsigned long int nb_chars = 0;
+	double nb_passwords = 0.0;
 	double entropy = 0.0;
-	char args = 0;
 
-	// opterr = 0;
-
-	while ((opt = getopt(argc, argv, "aeih")) != -1) {
+	// Add support for long options
+	while ((opt = getopt(argc, argv, "aeihr")) != -1) {
 		switch (opt) {
 		case 'a':
 			args |= ARGS_A;
@@ -60,6 +67,9 @@ int main(int argc, char *argv[])
 		case 'e':
 			args |= ARGS_E;
 			args |= ARGS_I;
+			break;
+		case 'r':
+			args |= ARGS_R;
 			break;
 		case 'h':
 			usage();
@@ -73,7 +83,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (!(args & ARGS_I))
-		printf("input: ");
+		printf("Input:              ");
 	while (1) {
 		ssize_t ret = 0;
 
@@ -81,8 +91,7 @@ int main(int argc, char *argv[])
 		if (ret == 0) {
 			break;
 		} else if (ret < 0) {
-			// What could this error be? Write it in the message
-			printf("Error while reading from stdin.\n");
+			printf("%s\n", strerror(errno));
 			usage();
 			goto end;
 		}
@@ -115,20 +124,26 @@ entropy:
 
 	if (!(args & ARGS_I))
 		printf("\n");
-	entropy = calculate_entropy(nb_unique_chars, nb_chars);
+
+	entropy = calculate_entropy(nb_unique_chars, nb_chars, &nb_passwords);
+	if (args & ARGS_R)
+		entropy = round(entropy);
 
 	// Definir des options d'output:
 	// 		- Nombre de chiffres après la virgule pour l'entreopy
-	// 		- Entreopy nombre entier
 
 	if (args & ARGS_A) {
-		printf("Characters: %lu\n", nb_chars);
-		printf("Unique characters: %lu\n", nb_unique_chars);
+		printf("Characters:         %lu\n", nb_chars);
+		printf("Unique characters:  %lu\n", nb_unique_chars);
+		printf("Possible passwords: %.0f\n", nb_passwords);
 	}
 	if (!(args & ARGS_E))
-		printf("Entropy: ");
-	printf("%f\n", entropy);
+		printf("Entropy:            ");
+	if (args & ARGS_R)
+		printf("%.0f\n", entropy);
+	else
+		printf("%f\n", entropy);
 
 end:
-	return 0;
+	exit(EXIT_SUCCESS);
 }
